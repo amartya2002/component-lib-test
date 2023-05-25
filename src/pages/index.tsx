45325b9cfd8cd7253b6c3274da5de368a26c9b70
Amartya Sinha <amartua2002@gmail.com>
2023-05-25 21:51:33 +0530

    Project Created

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+      <div className='text-7xl'>Hello</div>
+    </main>
+  )
+}
